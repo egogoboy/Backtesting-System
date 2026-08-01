a0cd621304a3863ad04f558164993fecf977b13f
egogoboy <egor2005sudakov@gmail.com>
2026-08-01 09:16:34 +0300

    feat(tests): implement ExecutionHandler.ExecuteOrder test

--- tests/test_execution_handler.cpp
+++ tests/test_execution_handler.cpp
@@ -1,6 +1,8 @@
 #include "backtester/config/ExecutionConfig.hpp"
 #include "backtester/config/PortfolioConfig.hpp"
 #include "backtester/core/EventQueue.hpp"
+#include "backtester/enums/EventType.hpp"
+#include "backtester/enums/OrderStatus.hpp"
 #include "backtester/events/OrderEvent.hpp"
 #include "backtester/execution_handler/ExecutionHandler.hpp"
 #include "backtester/models/MarketData.hpp"
@@ -57,4 +59,32 @@ TEST(ExecutionHandler, InvalidOrdersCreation) {
         Order::make_stop(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 1, 1.169, 1.169, 1.180))));
 
     EXPECT_LE(execution_handler.get_floating_risk(), 1e-7);
+}
+
+TEST(ExecutionHandler, ExecuteOrder) {
+    EventQueue event_queue;
+    PortfolioConfig portfolio_config(1, 10000);
+    Portfolio portfolio(portfolio_config);
+    MarketData initial_market_data(GLOBAL_EURUSD_INSTRUMENT, {1.172, 1.175, 1.169, 1.170}, 0);
+
+    ExecutionHandler execution_handler(conf, event_queue, portfolio, initial_market_data);
+
+    auto order = std::make_shared<Order>(
+        Order::make_market(GLOBAL_EURUSD_INSTRUMENT, Direction::LONG, 0.1, 1.169, 1.180));
+
+    execution_handler.on_order_event(std::make_shared<OrderEvent>(order));
+
+    MarketData next_market_data(GLOBAL_EURUSD_INSTRUMENT, {1.170, 1.174, 1.168, 1.172}, 1);
+
+    execution_handler.on_market_event(std::make_shared<MarketEvent>(next_market_data));
+
+    EXPECT_EQ(order->get_status(), OrderStatus::EXECUTED);
+
+    EXPECT_FALSE(event_queue.empty());
+
+    EXPECT_EQ(event_queue.front()->get_type(), EventType::FILL);
+
+    EXPECT_FALSE(portfolio.has_available_funds(10000));
+
+    EXPECT_TRUE(portfolio.has_available_funds(9000));
 }
