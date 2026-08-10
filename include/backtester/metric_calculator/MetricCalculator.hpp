627f54fb039e9a7a5f61a2feebe32ee2b332e7d8
egogoboy <egor2005sudakov@gmail.com>
2026-08-10 16:22:30 +0300

    feat(metrics): implement on_fill_event(), and metrics calculation
    Implement calculation of Total Amount of Trades, Amount of Winning Trades, Amount of Loss Trades, Gross Profit, Gross Loss, and Sum of R multipliers

--- include/backtester/metric_calculator/MetricCalculator.hpp
+++ include/backtester/metric_calculator/MetricCalculator.hpp
@@ -2,7 +2,9 @@
 
 #include "backtester/events/FillEvent.hpp"
 #include "backtester/events/MarketEvent.hpp"
+#include "backtester/models/Position.hpp"
 #include "backtester/portfolio/Portfolio.hpp"
+#include <cstdint>
 #include <functional>
 #include <memory>
 
@@ -15,9 +17,29 @@ class MetricCalculator {
     void on_fill_event(const std::shared_ptr<FillEvent> &event);
 
   private:
+    void update_total_amount_of_trades();
+
     void update_maximum_drawdown();
+
+    void update_win_and_loss(const Position &position);
+
+    void update_r_multipliers_sum(const Position &position);
+
+    static double calculate_r_multiplier(const Position &position);
 
     std::reference_wrapper<const Portfolio> portfolio_;
 
     double max_drawdown_;
+
+    uint32_t total_amount_of_trades_ = 0;
+
+    uint32_t amount_of_winning_trades_ = 0;
+
+    uint32_t amount_of_loss_trades_ = 0;
+
+    double gross_profit_ = 0;
+
+    double gross_loss_ = 0;
+
+    double r_multipliers_sum_ = 0;
 };
--- src/metric_calculator/MetricCalculator.cpp
+++ src/metric_calculator/MetricCalculator.cpp
@@ -1,5 +1,10 @@
 #include "backtester/metric_calculator/MetricCalculator.hpp"
+#include "backtester/enums/FillAction.hpp"
+#include "backtester/models/Order.hpp"
+#include "backtester/portfolio/Portfolio.hpp"
+#include "backtester/utils/PnLCalculator.hpp"
 #include <algorithm>
+#include <cstdlib>
 
 MetricCalculator::MetricCalculator(const Portfolio &portfolio)
     : portfolio_{portfolio}, max_drawdown_{portfolio.get_total_equity()} {}
@@ -8,8 +13,48 @@ void MetricCalculator::on_market_event(const std::shared_ptr<MarketEvent> &event
     update_maximum_drawdown();
 }
 
-void MetricCalculator::on_fill_event(const std::shared_ptr<FillEvent> &event) {}
+void MetricCalculator::on_fill_event(const std::shared_ptr<FillEvent> &event) {
+    if (event->get_action() == FillAction::OPEN) {
+        update_total_amount_of_trades();
+    }
+
+    if (event->get_action() == FillAction::CLOSE) {
+        const Position &position = *(event->get_data());
+
+        update_win_and_loss(position);
+
+        update_r_multipliers_sum(position);
+    }
+}
+
+void MetricCalculator::update_total_amount_of_trades() {
+    ++total_amount_of_trades_;
+}
 
 void MetricCalculator::update_maximum_drawdown() {
     max_drawdown_ = std::min(portfolio_.get().get_total_equity(), max_drawdown_);
+}
+
+void MetricCalculator::update_win_and_loss(const Position &position) {
+    double position_pnl = PnLCalculator::get_position_realized_pnl(position);
+
+    if (position_pnl > 0) {
+        ++amount_of_winning_trades_;
+        gross_profit_ += position_pnl;
+    } else {
+        ++amount_of_loss_trades_;
+        gross_loss_ -= position_pnl;
+    }
+}
+
+void MetricCalculator::update_r_multipliers_sum(const Position &position) {
+    r_multipliers_sum_ += calculate_r_multiplier(position);
+}
+
+double MetricCalculator::calculate_r_multiplier(const Position &position) {
+    double position_pnl = PnLCalculator::get_position_realized_pnl(position);
+    double entry_price = position.get_entry_price();
+    double stop_loss = position.get_stop_loss_order().lock()->get_trigger_price().value();
+
+    return position_pnl / std::abs(entry_price - stop_loss);
 }
