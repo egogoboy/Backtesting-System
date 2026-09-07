9c0f7f65e39eda45fa3d15ddf619b22d2d1d305a
egogoboy <egor2005sudakov@gmail.com>
2026-09-07 19:21:43 +0300

    feat(Engine): connect all modules to the Engine, finish queue flow

--- include/backtester/core/Engine.hpp
+++ include/backtester/core/Engine.hpp
@@ -1,24 +1,42 @@
 #pragma once
 
+#include "backtester/config/ExecutionConfig.hpp"
+#include "backtester/config/PortfolioConfig.hpp"
+#include "backtester/config/RiskManagerConfig.hpp"
 #include "backtester/core/EventQueue.hpp"
 #include "backtester/data_feed/DataFeed.hpp"
+#include "backtester/execution_handler/ExecutionHandler.hpp"
+#include "backtester/metric_calculator/MetricCalculator.hpp"
+#include "backtester/models/Metrics.hpp"
+#include "backtester/portfolio/Portfolio.hpp"
+#include "backtester/risk_manager/RiskManager.hpp"
 #include "backtester/strategy/Strategy.hpp"
 #include <chrono>
 #include <functional>
 
 class Engine {
   public:
-    Engine(Strategy &strategy, DataFeed &data_feed)
-        : start_time_{std::chrono::system_clock::now()}, strategy_{strategy},
-          data_feed_(data_feed) {
-        strategy_.get().set_event_queue(event_queue_);
+    Engine(const std::shared_ptr<Strategy> &strategy, DataFeed &data_feed,
+           PortfolioConfig &portfolio_config, RiskManagerConfig &risk_manager_config,
+           ExecutionConfig &execution_config)
+        : start_time_{std::chrono::system_clock::now()}, strategy_{strategy}, data_feed_(data_feed),
+          portfolio_(portfolio_config), risk_manager_(risk_manager_config, event_queue_, portfolio_,
+                                                      data_feed_.get().get_current_market_data()),
+          execution_handler_(execution_config, event_queue_, portfolio_,
+                             data_feed_.get().get_current_market_data()),
+          metric_calculator_(portfolio_) {
+        strategy_->set_event_queue(event_queue_);
     }
 
-    void run();
+    Metrics run();
 
   private:
     std::chrono::system_clock::time_point start_time_;
-    std::reference_wrapper<Strategy> strategy_;
+    std::shared_ptr<Strategy> strategy_;
     std::reference_wrapper<DataFeed> data_feed_;
+    Portfolio portfolio_;
+    RiskManager risk_manager_;
+    ExecutionHandler execution_handler_;
     EventQueue event_queue_;
+    MetricCalculator metric_calculator_;
 };
--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -1,8 +1,9 @@
 #include "backtester/core/Engine.hpp"
 #include "backtester/events/MarketEvent.hpp"
+#include "backtester/models/Metrics.hpp"
 #include <memory>
 
-void Engine::run() {
+Metrics Engine::run() {
     while (data_feed_.get().has_next()) {
         event_queue_.push(std::make_shared<MarketEvent>(data_feed_.get().get_next_market_event()));
         while (!event_queue_.empty()) {
@@ -11,18 +12,27 @@ void Engine::run() {
 
             switch (event->get_type()) {
             case EventType::MARKET:
-                strategy_.get().on_market(*std::static_pointer_cast<MarketEvent>(event));
+                execution_handler_.on_market_event(std::static_pointer_cast<MarketEvent>(event));
+                risk_manager_.on_market_event(std::static_pointer_cast<MarketEvent>(event));
+                metric_calculator_.on_market_event(std::static_pointer_cast<MarketEvent>(event));
+                strategy_->on_market(*std::static_pointer_cast<MarketEvent>(event));
                 break;
 
             case EventType::SIGNAL:
+                risk_manager_.on_signal_event(std::static_pointer_cast<SignalEvent>(event));
                 break;
 
             case EventType::ORDER:
+                execution_handler_.on_order_event(std::static_pointer_cast<OrderEvent>(event));
                 break;
 
             case EventType::FILL:
+                portfolio_.on_fill_event(std::static_pointer_cast<FillEvent>(event));
+                metric_calculator_.on_fill_event(std::static_pointer_cast<FillEvent>(event));
                 break;
             }
         }
     }
+
+    return metric_calculator_.calculate_metrics();
 }
